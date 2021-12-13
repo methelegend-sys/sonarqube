570e878bf61e79ef777f556bf0286486786a53a5
Ankit Kumar <55847022+methelegend-sys@users.noreply.github.com>
2021-12-13 12:39:45 +0530

    Update dependency-check.bat

--- dependency-check.bat
+++ dependency-check.bat
@@ -4,7 +4,7 @@ set DC_VERSION="latest"
 set DC_DIRECTORY=%USERPROFILE%\OWASP-Dependency-Check
 SET DC_PROJECT="dependency-check scan: %CD%"
 set DATA_DIRECTORY="%DC_DIRECTORY%\data"
-set REPORT_DIRECTORY="%DC_DIRECTORY%\report"
+set REPORT_DIRECTORY="%CD%\report"
 
 IF NOT EXIST %DATA_DIRECTORY% (
     echo Initially creating persistent directory: %DATA_DIRECTORY%
